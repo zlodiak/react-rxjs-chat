6ccb307ae5e47cb1d43af16f2c8041a00c9ec339
kalinin <prozaik81-2@yandex.ru>
2020-12-27 12:11:18 +0300

    chat for one person

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import  React  from  'react';
+import { BrowserRouter, Route, Switch } from  'react-router-dom';
+import { FirstPerson } from './components';
+import  './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const  App  = () => (
+  <BrowserRouter>
+    <>
+      <Switch>
+        <Route path="/" component={FirstPerson} exact />
+        <Route path="/first-person" component={FirstPerson} exact />
+      </Switch>
+    </>
+  </BrowserRouter>
+);
 
-export default App;
+export  default  App;
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+
+export { default as FirstPerson } from './FirstPerson';
